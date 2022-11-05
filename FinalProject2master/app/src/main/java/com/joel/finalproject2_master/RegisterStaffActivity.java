761ed3cb979f7dbf95d2a1019af43bf22cad15f1
JOEL RAYAPOH DAMANIK <joeldamanik@student.telkomuniversity.ac.id>
2022-11-06 02:20:31 +0700

    add staff

--- FinalProject2master/app/src/main/java/com/joel/finalproject2_master/RegisterStaffActivity.java
+++ FinalProject2master/app/src/main/java/com/joel/finalproject2_master/RegisterStaffActivity.java
@@ -1,22 +1,28 @@
 package com.joel.finalproject2_master;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class RegisterStaffActivity extends AppCompatActivity {
 
-    private Button backBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_staff);
-        backBtn=findViewById(R.id.btnBack);
 
-        backBtn.setOnClickListener(v->{
-            startActivity(new Intent(getApplicationContext(),MainActivity.class));
-        });
     }
 }

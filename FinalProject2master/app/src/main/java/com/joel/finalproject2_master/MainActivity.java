0e38ffbb31228c69e7d4b3425e4e9b74deb84b35
JOEL RAYAPOH DAMANIK <joeldamanik@student.telkomuniversity.ac.id>
2022-11-05 14:43:42 +0700

    add staff & add stock

--- FinalProject2master/app/src/main/java/com/joel/finalproject2_master/MainActivity.java
+++ FinalProject2master/app/src/main/java/com/joel/finalproject2_master/MainActivity.java
@@ -1,6 +1,7 @@
 package com.joel.finalproject2_master;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,27 +13,11 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-    private FirebaseUser firebaseUser;
-    private TextView textName;
-    private Button btnLogout;
+    CardView staffAdd,stockAdd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textName=findViewById(R.id.txt1);
-        firebaseUser=FirebaseAuth.getInstance().getCurrentUser();
-        btnLogout=findViewById(R.id.logoutBtn);
-        if(firebaseUser!=null){
-            textName.setText(firebaseUser.getDisplayName());
-        }else{
-            textName.setText("Login Gagal");
-        }
-
-        btnLogout.setOnClickListener(v->{
-            FirebaseAuth.getInstance().signOut();
-            startActivity(new Intent(getApplicationContext(), LoginAdminActivity.class));
-        });
-
     }
 
 }

11d61d9574087921f580d3ba52f6fdca179185c9
JOEL RAYAPOH DAMANIK <joeldamanik@student.telkomuniversity.ac.id>
2022-11-05 10:50:38 +0700

    login and register logic done

--- FinalProject2master/app/src/main/java/com/joel/finalproject2_master/MainActivity.java
+++ FinalProject2master/app/src/main/java/com/joel/finalproject2_master/MainActivity.java
@@ -3,12 +3,24 @@ package com.joel.finalproject2_master;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-
+    private FirebaseUser firebaseUser;
+    private TextView textName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textName=findViewById(R.id.txt1);
+        firebaseUser=FirebaseAuth.getInstance().getCurrentUser();
+        if(firebaseUser!=null){
+            textName.setText(firebaseUser.getDisplayName());
+        }else{
+            textName.setText("Login Gagal");
+        }
     }
 }
